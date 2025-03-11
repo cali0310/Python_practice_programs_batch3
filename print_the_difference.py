ae66cb06538e380b2ec6d3a7809314dd72447c7d
unknown <ramosyazmien2@gmail.com>
2025-03-12 02:10:20 +0800

    final output

--- print_the_difference.py
+++ print_the_difference.py
@@ -1,6 +1,4 @@
-# pseudocode  
-
-# input num1  
-# input num2  
-# difference = num1 - num2  
-# print difference  
+print("This program will print the difference of the two numbers.\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("\nThe difference of the two numbers is", num1-num2)
