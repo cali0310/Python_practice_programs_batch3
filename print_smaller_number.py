34d553c218da1b45c078d2fa1cd09b3daf80e8cb
unknown <ramosyazmien2@gmail.com>
2025-03-12 01:53:51 +0800

    final output

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,8 +1,7 @@
-# pseudocode
-
-# input num1
-# input num2
-# if num1 is equal to num2
-#     print "the numbers are equal"
-# else
-#     print "the smaller number is" min(num1, num2)
+print("This program will print the smaller number.")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num1 == num2:
+    print("\nThe numbers are equal.")
+else:
+    print("\nThe smaller number is ", min(num1, num2))
