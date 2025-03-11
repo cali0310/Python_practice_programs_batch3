7c44bc70b4c4c61997842057da0ccb111e7a1ed7
unknown <ramosyazmien2@gmail.com>
2025-03-12 00:46:29 +0800

    print output

--- print_if_equal_number.py
+++ print_if_equal_number.py
@@ -4,3 +4,12 @@
     #print the numbers are equal
 #else
     #print the numbers are not equal
+
+print("This program will ask you to enter two numbers and print 'EQUAL' if both numbers are the same.\n")
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+if number1==number2:
+    print("\nThe numbers are EQUAL.")
+else:
+    print("\nThe numbers are NOT EQUAL.")
