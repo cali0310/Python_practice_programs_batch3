e4e48705b7cc68593478022353e7e2367aaf17ba
unknown <ramosyazmien2@gmail.com>
2025-03-12 02:34:55 +0800

    pseudocode

---
+++ first_num_minus_all_remaining_num.py
@@ -0,0 +1,13 @@
+# pseudocode  
+
+# initialize list num  
+
+# loop from 0 to 9  
+    # input number and store in list num  
+# end loop  
+
+# sum_remaining = sum of numbers except the first number  
+# print sum_remaining  
+
+# result = first number - sum_remaining  
+# print result  
