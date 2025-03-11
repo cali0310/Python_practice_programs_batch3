0b9bcb4f3f1f6d19e8e2bbb38984eaffc8e32f4f
unknown <ramosyazmien2@gmail.com>
2025-03-12 00:58:00 +0800

    print output

--- print_the_sum.py
+++ print_the_sum.py
@@ -1,6 +1,4 @@
-#pseudocode
-
-#input num1  
-#input num2  
-#sum = num1 + num2  
-#print sum  
+print("This program will print the sum of the two numbers.\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("\nThe sum of the two numbers is", num1+num2)
