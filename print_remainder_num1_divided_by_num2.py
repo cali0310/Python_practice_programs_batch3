7914b1ae3a47df38c9793d0ea569b9c3b8f202d3
unknown <ramosyazmien2@gmail.com>
2025-03-12 02:28:09 +0800

    pseudocode

---
+++ print_remainder_num1_divided_by_num2.py
@@ -0,0 +1,12 @@
+# pseudocode  
+
+# input num1  
+# input num2  
+
+# while num2 is 0  
+    #print "Division by 0 is undefined. Try again."  
+    #input num2  
+# end while  
+
+# remainder = num1 % num2  
+# print remainder  
