f15852b5dba95820ae4f13dda64e5d9ca757a14a
unknown <ramosyazmien2@gmail.com>
2025-03-12 02:30:44 +0800

    final output

--- print_remainder_num1_divided_by_num2.py
+++ print_remainder_num1_divided_by_num2.py
@@ -1,12 +1,8 @@
-# pseudocode  
+print("This program will print the remainder of first number divided by the second number.\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+while num2==0:
+    print("Division by O will result to undefined. Try again.")
+    num2 = float(input("Enter the second number: "))
 
-# input num1  
-# input num2  
-
-# while num2 is 0  
-    #print "Division by 0 is undefined. Try again."  
-    #input num2  
-# end while  
-
-# remainder = num1 % num2  
-# print remainder  
+print("\nThe remainder of the two numbers is ", num1%num2)
