dc859caff2f0dac8bfc3063af09f24686aa2570d
unknown <ramosyazmien2@gmail.com>
2025-03-12 00:34:19 +0800

    print output

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,8 +1,4 @@
-#pseudocode
-
-#input num1
-#input num2
-#if num1 > num2
-    #print num1
-#else
-    #print num2
+print("This program will ask you to input two numbers and print the bigger number.\n")
+number1=float(input("Enter number 1: "))
+number2=float(input("Enter number 2: "))
+print("The bigger number is:", max(number1, number2))
