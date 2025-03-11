d3f987cb8ebe48a008613fd023f0e137f4bbb7ed
unknown <ramosyazmien2@gmail.com>
2025-03-12 02:03:02 +0800

    pseudocode

---
+++ print_if_not_equal.py
@@ -0,0 +1,8 @@
+# pseudocode
+
+# input num1  
+# input num2  
+# if num1 is not equal to num2  
+    #print "the entered numbers are not equal"  
+# else
+    #print "the entered numbers are equal"  
