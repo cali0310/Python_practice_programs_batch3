0f797f45fd16f5cd9e54c34dde8c4f6ed8f1c093
unknown <ramosyazmien2@gmail.com>
2025-03-12 01:09:46 +0800

    final output

--- print_the_quotient.py
+++ print_the_quotient.py
@@ -1,6 +1,8 @@
-# pseudocode  
-
-# input num1  
-# input num2  
-# quotient = num1 / num2  
-# print quotient  
+print("This program will print the quotient of the two numbers.\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num2 == 0:
+    print("The first number cannot be divided by 0.")
+    exit()
+else:
+    print("\nThe quotient of the two numbers is ", num1/num2)
