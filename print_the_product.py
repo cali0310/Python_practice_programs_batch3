5767aa8400cf2cd60a200cdaffefd7b7e4bb5dd8
unknown <ramosyazmien2@gmail.com>
2025-03-12 01:03:36 +0800

    print output

--- print_the_product.py
+++ print_the_product.py
@@ -1,5 +1,4 @@
-# pseudocode
-# input num1  
-# input num2  
-# product = num1 * num2  
-# print product
+print("This program will print the product of the two numbers.\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("\nThe product of the two numbers is ", num1*num2)
