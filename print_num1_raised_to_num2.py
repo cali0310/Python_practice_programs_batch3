16c6756c0372ac967143aa3f5d2c79ebc311c9bc
unknown <ramosyazmien2@gmail.com>
2025-03-12 01:17:12 +0800

    final output

--- print_num1_raised_to_num2.py
+++ print_num1_raised_to_num2.py
@@ -1,6 +1,4 @@
-# pseudocode  
-
-# input num1  
-# input num2  
-# result = num1 ** num2  
-# print result  
+print("This program will print the result of the first number raised to the second number.\n")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("\nThe result is ", num1**num2)
