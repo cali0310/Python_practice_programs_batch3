369aa8b3e5a0d271576c38349ec71309c9cfbfcb
unknown <ramosyazmien2@gmail.com>
2025-03-12 13:27:18 +0800

    final output

--- print_count_of_even_numbers.py
+++ print_count_of_even_numbers.py
@@ -1,10 +1,15 @@
- # pseudocode  
+print("This program will print the number of even numbers from the entered values.\n")
 
-# print message  
-# initialize count = 0  
-# loop from 1 to 10  
-#     input number  
-#     if number is even  
-#         count = count + 1  
-# end loop  
-# print count
+numbers = []
+
+for i in range(10):
+    number = int(input(f"Enter number {i+1}: "))
+    numbers.append(number)
+
+even = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even += 1
+
+print("\nThe number of even numbers entered is:", even)
