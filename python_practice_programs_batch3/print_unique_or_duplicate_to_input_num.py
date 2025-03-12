a398fe25cb91afc677608450e2d9a63ccd8a6572
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:35:43 +0800

    pseudocode

---
+++ python_practice_programs_batch3/print_unique_or_duplicate_to_input_num.py
@@ -0,0 +1,11 @@
+# pseudocode
+
+# input num
+# create an empty list numbers
+# loop until invalid input
+    # if num is not in numbers
+        # print "Unique"
+        # add num to numbers
+    # else
+        # print "Duplicate"
+    # input num
