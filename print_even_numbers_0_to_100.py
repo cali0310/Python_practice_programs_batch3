947832a81371d408ae5dc9a5b0932b70b2ce06d4
unknown <ramosyazmien2@gmail.com>
2025-03-12 14:27:17 +0800

    compilation of batch 1 programs

--- print_even_numbers_0_to_100.py
+++
@@ -1,6 +0,0 @@
-print("This program will print the even numbers from 0 to 100.")
-
-i = 0
-while i <= 100:
-    print(i, end=' ')
-    i += 2  
--- python_practice_programs_batch1/print_if_equal_number.py
+++
@@ -1,8 +0,0 @@
-print("This program will ask you to enter two numbers and print 'EQUAL' if both numbers are the same.\n")
-number1 = float(input("Enter the first number: "))
-number2 = float(input("Enter the second number: "))
-
-if number1==number2:
-    print("\nThe numbers are EQUAL.")
-else:
-    print("\nThe numbers are NOT EQUAL.")
