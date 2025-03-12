bff8225cf1c34123215161ecb9dc5bb4c6b2e6fa
unknown <ramosyazmien2@gmail.com>
2025-03-12 23:56:18 +0800

    ask user input and print output

--- python_practice_prpgrams_batch4/print_average.py
+++ python_practice_prpgrams_batch4/print_average.py
@@ -1,7 +1,15 @@
-# pseudocode
-# declare numList  
-# loop until input is invalid  
-    # input num  
-    # append to numList  
-# compute average  
-# print average  
+print("This program will ask the user to input a number until the user input is invalid and will display the average.\n")
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break  
+
+if num_list:
+    average = sum(num_list) / len(num_list)
+    print("Average of entered numbers:", average)
+else:
+    print("No valid numbers entered.")
