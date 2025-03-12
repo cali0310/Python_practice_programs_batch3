0cb210a150207d4de4709fc4672a2b1140bd773c
unknown <ramosyazmien2@gmail.com>
2025-03-12 13:24:18 +0800

    pseudocode

---
+++ print_count_of_even_numbers.py
@@ -0,0 +1,10 @@
+ # pseudocode  
+
+# print message  
+# initialize count = 0  
+# loop from 1 to 10  
+#     input number  
+#     if number is even  
+#         count = count + 1  
+# end loop  
+# print count
