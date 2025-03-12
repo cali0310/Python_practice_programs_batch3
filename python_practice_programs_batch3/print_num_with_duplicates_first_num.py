4878d17a49ff6b359a85d476057049cb3aa86716
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:23:22 +0800

    pseudocode

---
+++ python_practice_programs_batch3/print_num_with_duplicates_first_num.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# input num1 to 10
+# store all numbers in a list
+# create an empty set seen_numbers
+# loop through list
+    # if current number is not in seen_numbers
+        # print current number
+        # add current number to seen_numbers
