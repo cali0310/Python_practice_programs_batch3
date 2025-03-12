a719d05bbe0d3a64022ff177f6ceecf68f4cad84
unknown <ramosyazmien2@gmail.com>
2025-03-12 14:43:12 +0800

    final output

--- print_numbers_except_ending_in_0_and_5.py
+++ print_numbers_except_ending_in_0_and_5.py
@@ -1,7 +1,6 @@
 print("This program will print all the numbers starting from 0 to 100 except numbers ending in zero or ending in five.\n")
 
 i = 0
-
 while i <= 100:  
     if i % 10 != 0 and i % 10 != 5:  
         print(i, end=' ')  
