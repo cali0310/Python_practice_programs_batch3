d52062ff2c42ced19fdf1ad82babdf4355c1444e
unknown <ramosyazmien2@gmail.com>
2025-03-12 19:41:56 +0800

    pseudocode

---
+++ python_practice_prpgrams_batch4/print_duplicate_numbers.py
@@ -0,0 +1,7 @@
+# pseudocode
+# input numbers_list (empty list)
+# repeat 10 times:
+    # input num
+    # append num to numbers_list
+# process find duplicates in numbers_list
+# print duplicate numbers
