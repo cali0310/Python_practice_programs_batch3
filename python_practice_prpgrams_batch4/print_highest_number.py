4b08cb74f5269de158a8bb04eaf48d041235474a
unknown <ramosyazmien2@gmail.com>
2025-03-12 23:49:17 +0800

    ask user input and print output

--- python_practice_prpgrams_batch4/print_highest_number.py
+++ python_practice_prpgrams_batch4/print_highest_number.py
@@ -1,7 +1,14 @@
-# pseudocode
-# declare numList  
-# loop until input is invalid  
-    # input num  
-    # append to numList  
-# find highest number  
-# print highest number  
+print("This program will ask the user to input a number until the user input is invalid and will display the highest number.\n")
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break  
+
+if num_list:
+    print("Highest number:", max(num_list))
+else:
+    print("No valid numbers entered.")
