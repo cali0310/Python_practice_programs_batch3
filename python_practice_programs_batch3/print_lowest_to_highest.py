556853447c9a97e53b8a2e6ab6242ca1dc2c6106
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:49:25 +0800

    print values from lowest to highest

--- python_practice_programs_batch3/print_lowest_to_highest.py
+++ python_practice_programs_batch3/print_lowest_to_highest.py
@@ -1,8 +1,17 @@
-# pseudocode
-# input num1 to 10
-# create a list numbers
-# loop until invalid input
-    # add num to numbers
-    # input num1 to 10
-# sort the list numbers
-# print numbers
+print("This program will continue asking until the user input is invalid.\n It will display the number from lowest to highest.\n")
+
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] > numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+print("Numbers sorted from lowest to highest value:", numbers)
