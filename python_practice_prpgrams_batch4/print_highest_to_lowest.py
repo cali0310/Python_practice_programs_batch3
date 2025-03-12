92dd569872775861d2faacf138cdd10790461ca7
unknown <ramosyazmien2@gmail.com>
2025-03-12 23:52:50 +0800

    ask the user input and print output

--- python_practice_prpgrams_batch4/print_highest_to_lowest.py
+++ python_practice_prpgrams_batch4/print_highest_to_lowest.py
@@ -1,7 +1,14 @@
-# pseudocode
-# declare numList  
-# loop until input is invalid  
-    # input num  
-    # append to numList  
-# sort numList descending  
-# print numList  
+print("This program will ask the user to input a number until the user input is invalid and will display the number from highest to lowest.\n")
+num_list = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break  
+
+if num_list:
+    print("Numbers sorted (highest to lowest):", sorted(num_list, reverse=True))
+else:
+    print("No valid numbers entered.")
