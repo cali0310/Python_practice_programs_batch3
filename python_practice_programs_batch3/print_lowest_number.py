1104e814e885ba98e4c48ced91756a17a1c7b056
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:43:15 +0800

    pseudocode

---
+++ python_practice_programs_batch3/print_lowest_number.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# input num1 to 10
+# create a list numbers
+# loop until invalid input
+    # add num to numbers
+    # input num1 to 10
+# find the minimum value in numbers
+# print the minimum value
