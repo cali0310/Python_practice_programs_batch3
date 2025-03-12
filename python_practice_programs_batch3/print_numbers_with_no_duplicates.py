982b34419bcd9651c68de972a6de60aaac7d07e7
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:06:45 +0800

    compilation of practice programs batch 3

---
+++ python_practice_programs_batch3/print_numbers_with_no_duplicates.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# declare numList as empty list  
+# loop 10 times  
+    # input num  
+    # append num to numList  
+# loop through numList  
+    # if num appears only once in numList   
+        # print num  
