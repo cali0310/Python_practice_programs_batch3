d6d774d67cf37734fb364274b345c306f46f1616
Yazmien Hannah Ramos <ramosyazmien2@gmail.com>
2025-03-12 16:53:54 +0800

    Delete print_the_difference.py

--- print_the_difference.py
+++
@@ -1,4 +0,0 @@
-print("This program will print the difference of the two numbers.\n")
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-print("\nThe difference of the two numbers is", num1-num2)
