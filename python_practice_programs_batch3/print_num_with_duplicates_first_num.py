8c318d8ee6b0fbf31d33288636fe6d48c92786ab
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:31:02 +0800

    print final output

--- python_practice_programs_batch3/print_num_with_duplicates_first_num.py
+++ python_practice_programs_batch3/print_num_with_duplicates_first_num.py
@@ -1,6 +1,13 @@
 print("This program will display all numbers. For numbers with duplicate, display only the first entry.\n")
 
 numbers = []
-duplicates = set()
+duplicate = set()
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
+
+    if num not in duplicate:
+        numbers.append(num)
+
+    duplicate.add(num)
+
+print("First occurrences of numbers displayed:", numbers)
