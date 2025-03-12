e9576848fa46c9c063a72ef8acbdf9315f9d1725
unknown <ramosyazmien2@gmail.com>
2025-03-12 23:45:14 +0800

    ask user input and print output

--- python_practice_prpgrams_batch4/print_number_with_most_duplicate.py
+++ python_practice_prpgrams_batch4/print_number_with_most_duplicate.py
@@ -1,8 +1,15 @@
-# pseudocode
+print("This program will ask the user to input a number until the user input is invalid and will display the number with the most number of duplicate.\n")
+num_list = []
 
-# declare numList  
-# loop until input is invalid  
-    # input num  
-    # append to numList  
-# find number with most duplicates  
-# print number  
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
+    except ValueError:
+        break  
+
+if num_list:
+    most_common = max(set(num_list), key=num_list.count)
+    print("Number with most duplicates:", most_common)
+else:
+    print("No valid numbers entered.")
