303ab6537986a5bd3beaa3e5ca62bbf64bd637e3
unknown <ramosyazmien2@gmail.com>
2025-03-12 22:35:52 +0800

    ask user input and print duplicate

--- python_practice_prpgrams_batch4/print_duplicate_numbers.py
+++ python_practice_prpgrams_batch4/print_duplicate_numbers.py
@@ -1,7 +1,16 @@
-# pseudocode
-# input numbers_list (empty list)
-# repeat 10 times:
-    # input num
-    # append num to numbers_list
-# process find duplicates in numbers_list
-# print duplicate numbers
+print("This program will ask user to input 10 numbers and display all numbers that have duplicate.\n")
+numbers = []
+for i in range(10):
+    num = int(input("Enter number " + str(i + 1) + ": "))
+    numbers.append(num)
+
+duplicates = []
+for i in range(len(numbers)):
+    count = 0
+    for j in range(len(numbers)):
+        if numbers[i] == numbers[j]:
+            count += 1
+    if count > 1 and numbers[i] not in duplicates:
+        duplicates.append(numbers[i])
+
+print("Duplicate numbers are:", duplicates)
