9fe839f9dd096bb9ad613c1f602236a7af2e04cd
unknown <ramosyazmien2@gmail.com>
2025-03-12 14:49:29 +0800

    final output

--- print_numbers_between_two_num.py
+++ print_numbers_between_two_num.py
@@ -1,6 +1,8 @@
-# input num1  
-# input num2  
-# if num1 > num2  
-    # swap num1 and num2  
-# loop from num1 + 1 to num2 - 1  
-    # print i  
+print("This program will print the numbers between two numbers.")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 > num2:
+    num1, num2 = num2, num1  # Ensure correct order
+for i in range(num1 + 1, num2):
+    print(i, end=' ')
