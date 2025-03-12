a0f5160555bcb1087ccb99e69e56a5d55c3586e1
unknown <ramosyazmien2@gmail.com>
2025-03-12 17:16:43 +0800

    final output

--- python_practice_programs_batch3/print_numbers_with_no_duplicates.py
+++ python_practice_programs_batch3/print_numbers_with_no_duplicates.py
@@ -1,9 +1,17 @@
-# pseudocode
+print("This program will print the numbers without duplicates.\n")
 
-# declare numList as empty list  
-# loop 10 times  
-    # input num  
-    # append num to numList  
-# loop through numList  
-    # if num appears only once in numList   
-        # print num  
+numbers = []
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+
+unique_numbers = []
+for i in range(len(numbers)):
+    count = 0
+    for j in range(len(numbers)):
+        if numbers[i] == numbers[j]:
+            count += 1
+    if count == 1:
+        unique_numbers.append(numbers[i])
+
+print("\nNumbers without duplicates:", unique_numbers)
